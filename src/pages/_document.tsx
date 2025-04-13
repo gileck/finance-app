553b4070dee0aac21bd0a6188731cf756c789c37
Gil <gil@Gils-MacBook-Pro.local>
2025-04-13 18:58:21 +0300

    commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,9 +14,9 @@ export default function Document() {
         <meta name="theme-color" content="#000000" />
 
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/icons/icon-152x152.png" />
         <link rel="apple-touch-icon" sizes="152x152" href="/icons/icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/icon-152x152.png" />
         <link rel="apple-touch-icon" sizes="167x167" href="/icons/icon-152x152.png" />
 
         {/* iOS splash screens */}
